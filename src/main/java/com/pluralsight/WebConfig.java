cffca3f1f4826a5fe39aee366791c95eac5632ec
handanunal <handan.unal@yahoo.com>
2019-10-23 21:56:29 +0300

    Resolving a view

--- src/main/java/com/pluralsight/WebConfig.java
+++ src/main/java/com/pluralsight/WebConfig.java
@@ -1,12 +1,24 @@
 package com.pluralsight;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@Configuration    /*We don't need a web xml,create a servlet.. Spring automatically creates context*/
+@Configuration /*
+				 * We don't need a web xml,create a servlet.. Spring automatically creates
+				 * context
+				 */
 @EnableWebMvc
-@ComponentScan(basePackages= "com.pluralsight")
+@ComponentScan(basePackages = "com.pluralsight")
 public class WebConfig {
 
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
 }
