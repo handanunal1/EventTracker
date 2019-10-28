355b7cd24077220186cac9dbc7ea1cc7e4b302d9
handanunal <handan.unal@yahoo.com>
2019-10-28 18:30:05 +0300

    Resolving a view - version difference

--- src/main/java/com/pluralsight/WebAppInitializer.java
+++ src/main/java/com/pluralsight/WebAppInitializer.java
@@ -14,22 +14,18 @@ public class WebAppInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-
 		WebApplicationContext context = getContext();
 		servletContext.addListener(new ContextLoaderListener(context));
-		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet",
-				new DispatcherServlet(context));
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("*.html");
 		dispatcher.addMapping("*.pdf");
-		dispatcher.addMapping("*.css");
-
 	}
 
 	private AnnotationConfigWebApplicationContext getContext() {
 		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-		// context.setConfigLocation("com.pluralsight.WebConfig");
-		context.register(WebConfig.class);
+		context.setConfigLocation("com.pluralsight.WebConfig");
+		   context.register(com.pluralsight.WebConfig.class);
 		return context;
 	}
 
--- src/main/java/com/pluralsight/controller/EventConroller.java
+++ src/main/java/com/pluralsight/controller/EventConroller.java
@@ -2,6 +2,7 @@ package com.pluralsight.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,4 +19,13 @@ public class EventConroller {
 
 		return "event";
 	}
+
+	@RequestMapping(value = "/event", method = RequestMethod.POST)
+	public String processEventPage(@ModelAttribute("event") Event event) {
+
+		System.out.println(event);
+
+		return "event";
+	}
+
 }
